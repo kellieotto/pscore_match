4d01d7493088ccb801eff9f32d633d0c47582cf9
Kellie Ottoboni <kellieotto@berkeley.edu>
2017-07-10 15:27:40 -0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,85 @@
+#! /usr/bin/env python
+
+descr = """Propensity score matching
+"""
+
+import os
+
+
+DISTNAME = 'pscore-match'
+DESCRIPTION = 'Propensity score matching'
+LONG_DESCRIPTION = descr
+AUTHOR = 'Kellie Ottoboni'
+AUTHOR_EMAIL = 'kellieotto@berkeley.edu'
+URL = 'http://www.github.com/kellieotto/pscore-match'
+LICENSE = 'BSD 2 License'
+DOWNLOAD_URL = 'http://www.github.com/kellieotto/pscore-match'
+VERSION = '0.1dev'
+PYTHON_VERSION = (2, 7)
+
+INSTALL_REQUIRES = [
+    'numpy',
+    'scipy'
+]
+
+TESTS_REQUIRE = [
+    'coverage',
+    'nose',
+    'flake8'
+]
+
+
+def write_version_py(filename='pscore-match/version.py'):
+    template = """# THIS FILE IS GENERATED FROM THE PSCORE-MATCH SETUP.PY
+version='%s'
+"""
+
+    try:
+        fname = os.path.join(os.path.dirname(__file__), filename)
+        with open(fname, 'w') as f:
+            f.write(template % VERSION)
+    except IOError:
+        raise IOError("Could not open/write to pscore-match/version.py - did you "
+                      "install using sudo in the past? If so, run\n"
+                      "sudo chown -R your_username ./*\n"
+                      "from package root to fix permissions, and try again.")
+
+
+if __name__ == "__main__":
+
+    write_version_py()
+
+    from setuptools import setup
+    setup(
+        name=DISTNAME,
+        version=VERSION,
+        license=LICENSE,
+        description=DESCRIPTION,
+        long_description=LONG_DESCRIPTION,
+        author=AUTHOR,
+        author_email=AUTHOR_EMAIL,
+        url=URL,
+        download_url=DOWNLOAD_URL,
+
+        classifiers=[
+            'Development Status :: 3 - Alpha',
+            'Environment :: Console',
+            'Intended Audience :: Developers',
+            'Intended Audience :: Science/Research',
+            'License :: OSI Approved :: BSD License',
+            'Programming Language :: Python',
+            'Programming Language :: Python :: 2.7',
+            'Programming Language :: Python :: 3.4',
+            'Programming Language :: Python :: 3.5',
+            'Topic :: Scientific/Engineering',
+            'Operating System :: Microsoft :: Windows',
+            'Operating System :: POSIX',
+            'Operating System :: Unix',
+            'Operating System :: MacOS',
+        ],
+
+        install_requires=INSTALL_REQUIRES,
+        tests_require=TESTS_REQUIRE,
+
+        packages=['pscore-match', 'pscore-match.tests',],
+    )
