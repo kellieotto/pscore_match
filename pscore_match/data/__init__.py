ca57ae9721aab6db75c846957f9ce6f170a89d2e
Kellie Ottoboni <kellieotto@berkeley.edu>
2017-07-14 16:14:37 -0700

    Create data directory and load data functions

---
+++ pscore_match/data/__init__.py
@@ -0,0 +1,55 @@
+"""Standard test data."""
+
+from __future__ import (absolute_import, division,
+                        print_function, unicode_literals)
+import os as _os
+import numpy as np
+
+__all__ = ['gerber_green_imai',
+           'dehejia_wahba']
+
+def gerber_green_imai():
+    """
+    This is the dataset from Imai (2005) used to replicate and evaluate
+    the field experiment done by Gerber and Green (2000).
+    
+    Notes
+    -----
+    .. Gerber, Alan S. and Donald P. Green. 2000. "The effects of canvassing,
+    telephone calls, and direct mail on voter turnout: a field experiment."
+    American Political Science Review 94: 653-663.
+    
+    .. Gerber, Alan S. and Donald P. Green. 2005. "Correction to Gerber and Green (2000),
+    replication of disputed findings, and reply to Imai (2005)." American Political 
+    Science Review 99: 301-313.
+    
+    .. Imai, Kosuke. 2005. "Do get-out-the-vote calls reduce turnout? The importance of 
+    statistical methods for field experiments." American Political Science Review 99: 
+    283-300.
+    """
+    return pd.read_table('../data/GerberGreenImai.txt', sep = '\s+')
+
+
+def dehejia_wahba():
+    """
+    Data from Dehejia and Wahba (1999, 2002) used to replicate and evaluate the matching
+    results of Lalonde (1986).
+
+    .. Dehejia, Rajeev and Sadek Wahba. 1999. "Causal effects in non-experimental studies: 
+    Reevaluating the evaluation of training programs." Journal of the American Statistical
+    Association 94 (448): 1053-1062.
+
+    .. Dehejia, Rajeev and Sadek Wahba. 2002. "Propensity score matching methods for non-
+    experimental causal studies." Review of Economics and Statistics 84: 151-161.
+    
+    .. LaLonde, Robert. 1986. "Evaluating the econometric evaluations of training programs 
+    with experimental data." American Economic Review 76 (4): 604-620.
+    """
+    names = ['Treated', 'Age', 'Education', 'Black', 'Hispanic', 'Married',
+             'Nodegree', 'RE74', 'RE75', 'RE78']
+    treated = pd.read_table('../data/nswre74_treated.txt', sep = '\s+',
+                            header = None, names = names)
+    control = pd.read_table('../data/nswre74_control.txt', sep='\s+', 
+                            header = None, names = names)
+    data = pd.concat([treated, control])
+    return data
